d86e0cdbdd7b670675cc084c2915690427ae1dd1
dhandynuzirwan <dhandynuzirwan@gmail.com>
2026-08-19 15:12:28 +0700

    refactor: add xml documentation to IRepository interface

--- Repositories/Interfaces/IRepository.cs
+++ Repositories/Interfaces/IRepository.cs
@@ -3,6 +3,10 @@ using System.Threading.Tasks;
 
 namespace InventorySystem.Repositories;
 
+/// <summary>
+/// Generic repository interface providing standard CRUD operations.
+/// </summary>
+/// <typeparam name="T">The entity type</typeparam>
 public interface IRepository<T> where T : class
 {
     Task<IEnumerable<T>> GetAllAsync();
